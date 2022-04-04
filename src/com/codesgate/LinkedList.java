ec46163eaf95f563bf0af165a87b8afac6168465
CodeCures <couragesblog@gmail.com>
2022-04-04 21:33:30 +0100

    Initial commit: create LinkedList class with Node inner class, addLast and addFirst methods that adds elements to the start and end of the list

---
+++ src/com/codesgate/LinkedList.java
@@ -0,0 +1,56 @@
+package com.codesgate;
+
+public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+
+    public void addLast(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = node;
+        else
+            last = last.next = node;
+    }
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+
+        if(isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+    }
+
+    public int indexOf(int item){
+        int index  = 0;
+        var current = first;
+
+        while(current != null){
+            if(current.value == item) return index;
+
+            current = last;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item){
+        return indexOf(item) != -1;
+    }
+
+    private boolean isEmpty(){
+        return first == null;
+    }
+}
---
+++ src/com/codesgate/Main.java
@@ -0,0 +1,13 @@
+package com.codesgate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    var list = new LinkedList();
+
+        list.addLast(10);
+        list.addLast(20);
+        list.addFirst(50);
+        list.addLast(30);
+    }
+}
