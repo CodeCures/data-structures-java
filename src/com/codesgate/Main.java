51b7b638b7100365a1795d86867fe975d68dcc40
CodeCures <couragesblog@gmail.com>
2022-04-05 17:50:56 +0100

    Add reverse method

--- src/com/codesgate/Main.java
+++ src/com/codesgate/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	    var list = new LinkedList();
 
-//        list.addLast(10);
-//        list.addLast(20);
-//        list.addLast(30);
-//        list.addLast(50);
-        list.removeFirst();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(50);
+        list.reverse();
     }
 }
